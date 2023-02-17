033b8346652a6eeebb5e3c31d57ec85d9465afc1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-18 00:02:49 +0600

    Creating Drawer Page

---
+++ lib/pages/drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DrawerPage extends StatelessWidget {
+  static const String routeName ='/drawer';
+  const DrawerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
