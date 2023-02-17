511b53346da94c45de5fcf06411929bdbe44a14e
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-17 17:16:12 +0600

    Updating Row

--- lib/main.dart
+++ lib/main.dart
@@ -23,53 +23,52 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Center(
-                  child: Container(
-                    height: 250,
-                    width: 250,
-                    //transform: Matrix4.rotationZ(.2),
-                    decoration:  BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(10.0),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.red,
+                height: 250,
+                child: Center(child: Text('Container 1')),
+              ),
+            ),
+/*            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.green,
+                height: 250,
+                child: Center(child: Text('Container 2')),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.grey,
+                height: 250,
+                child: Center(child: Text('Container 3')),
+              ),
+            ),*/
+            Expanded(
+              child: Center(
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  //transform: Matrix4.rotationZ(.2),
+                  decoration:  BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10.0),
                       image: DecorationImage(
                         fit: BoxFit.cover,
                         image: NetworkImage('https://images.fineartamerica.com/public/images/overview/homeDecor/bathTowels001.jpg'),
                       ),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 100,
-                        ),
+                        color: Colors.grey,
+                        blurRadius: 100,
+                      ),
                       ],
-                    ),
-                    child: const Center(child: Text('Log In')),
                   ),
+                  child: const Center(child: Text('Log In')),
                 ),
-              ],
-            ),
-
-            Center(
-              child: Container(
-                height: 250,
-                width: 250,
-                transform: Matrix4.rotationZ(.2),
-                decoration:  BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(10.0),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage('https://images.fineartamerica.com/public/images/overview/homeDecor/bathTowels001.jpg'),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 100,
-                    ),
-                    ],
-                ),
-                child: const Center(child: Text('Log In')),
               ),
             ),
           ],
