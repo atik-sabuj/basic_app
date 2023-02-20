68c761492c69360841e08493c65d0117c4cb48a8
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-20 15:58:18 +0600

    Basic Pub

--- lib/pages/pub.dart
+++ lib/pages/pub.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-class PubPage extends StatelessWidget {
+class PubPage extends StatefulWidget {
   const PubPage({Key? key}) : super(key: key);
 
+  @override
+  State<PubPage> createState() => _PubPageState();
+}
+
+class _PubPageState extends State<PubPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pub Page'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+        child: Column(
+          /*mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,*/
+          children: [
+            Icon(Icons.chat_bubble),
+            Icon(Icons.add),
+            Icon(Icons.home_filled),
+          ],
+        ),
       ),
     );
   }
