2b2a726b9d8472bbeccee94d8ce5e8c74a24a5fa
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-21 16:40:06 +0600

    PinCode Text Field

--- lib/pages/pub_dev/pub.dart
+++ lib/pages/pub_dev/pub.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:readmore/readmore.dart';
 import 'package:badges/badges.dart' as badges;
 import 'package:animated_text_kit/animated_text_kit.dart';
@@ -16,7 +17,7 @@ class _PubPageState extends State<PubPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      //backgroundColor: Colors.teal,
       appBar: AppBar(
         title: Text('Pub Page'),
         actions: [
@@ -31,85 +32,28 @@ class _PubPageState extends State<PubPage> {
         ],
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-                child: badges.Badge(
-              badgeContent: Text('4'),
-              child: Icon(Icons.shopping_cart_checkout_outlined),
-            )),
-            SizedBox(
-              width: 10,
-            ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const SizedBox(width: 20.0, height: 100.0),
-                    const Text(
-                      'Be',
-                      style: TextStyle(fontSize: 43.0),
-                    ),
-                    const SizedBox(width: 20.0, height: 100.0),
-                    DefaultTextStyle(
-                      style: const TextStyle(
-                        fontSize: 40.0,
-                        fontFamily: 'Horizon',
-                      ),
-                      child: AnimatedTextKit(
-                        animatedTexts: [
-                          RotateAnimatedText('AWESOME'),
-                          RotateAnimatedText('OPTIMISTIC'),
-                          RotateAnimatedText('DIFFERENT'),
-                        ],
-                        onTap: () {
-                          print("Tap Event");
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-            SizedBox(
-              width: 250.0,
-              child: DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 30.0,
-                  fontFamily: 'Bobbers',
-                ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    TyperAnimatedText('It is not enough to do your best,'),
-                    TyperAnimatedText('you must know what to do,'),
-                    TyperAnimatedText('and then do your best'),
-                    TyperAnimatedText('- W.Edwards Deming'),
-                  ],
-                  onTap: () {
-                    print("Tap Event");
-                  },
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              PinCodeTextField(
+                  appContext: context,
+                  length: 6,
+                  keyboardType: TextInputType.number,
+                  pinTheme: PinTheme(
+                    shape: PinCodeFieldShape.box,
+                        borderRadius: BorderRadius.circular(10),
+                    fieldHeight: 50,
+                    fieldWidth: 40,
+                  ),
+                  onChanged: (value){
+
+                  }
               ),
-            ),
-            SizedBox(
-              width: 250.0,
-              child: DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 70.0,
-                  fontFamily: 'Canterbury',
-                ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    ScaleAnimatedText('Think'),
-                    ScaleAnimatedText('Build'),
-                    ScaleAnimatedText('Ship'),
-                  ],
-                  onTap: () {
-                    print("Tap Event");
-                  },
-                ),
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
